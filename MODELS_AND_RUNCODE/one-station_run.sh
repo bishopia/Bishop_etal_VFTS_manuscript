401a1e605d8bf89e76cbedc1d7a4736d6bc2fa2c
bishopia <iwbishop@gmail.com>
2025-09-26 10:02:42 -0700

    Create one-station_run.sh

---
+++ MODELS_AND_RUNCODE/one-station_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=mv51_highIter
+#SBATCH --nodes=1
+#SBATCH --mem=128G
+#SBATCH --ntasks=4
+#SBATCH --partition=cpu
+#SBATCH --account=swbsc
+source activate /home/ibishop/miniforge3/envs/sM
+Rscript mv58_onechunk.r
